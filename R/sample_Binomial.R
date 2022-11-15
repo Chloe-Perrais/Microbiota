b0d04c8d392a544fc2e684bf564b1d60086ef1d0
DavidGJ04 <102814815+DavidGJ04@users.noreply.github.com>
2022-11-15 17:19:17 +0100

    :elephant: sample the number of individuals in each habitat

--- R/sample_Binomial.R
+++ R/sample_Binomial.R
@@ -1,11 +1,18 @@
 #' @title Sampling function for the abundances of species in different habitats
-#' @param Nhabitats prob nindiviual_per_habitat
+#' @param Nhabitats number of habitats (could be either a vector of values or a single value that will be recycle)
+#' @param prob frequency of OTU1 (could be either a vector of values or a single value that will be recycle)
+#' @param mean_nindiviuals_per_habitat average number of individuals per habitat (could be either a vector of values or a single value that will be recycle)
 #' @description Functions that randomly sample abundance data in N habitat and return the abundance table
 #'
 #' @return dataframe
 #' @export
-sample_Binomial <- function(Nhabitats, prob, nindiviual_per_habitat){
-  data <- rbinom(n=Nhabitats, p=prob, size=nindiviual_per_habitat)
+sample_Binomial <- function(Nhabitats, prob, mean_nindiviuals_per_habitat){
+  if(length(mean_nindiviuals_per_habitat)==1){
+    count <- rpois(n=Nhabitats, lambda = mean_nindiviuals_per_habitat)
+  data <- rbinom(n=Nhabitats, p=prob, size=count)
+  }else{
+    data <- rbinom(n=Nhabitats, p=prob, size=mean_nindiviuals_per_habitat)
+  }
   data <- rbind(data,nindiviual_per_habitat-data)
   colnames(data)=paste0("sample_",1:100)
   rownames(data)= c("OTU1","OTU2")
